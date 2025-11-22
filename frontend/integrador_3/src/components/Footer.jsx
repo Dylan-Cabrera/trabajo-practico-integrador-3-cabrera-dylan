9fe197ad40be322cb93587faac499d1d71c15746
DylanC <cabreradylan055@gmail.com>
2025-11-22 19:41:09 -0300

    Navbar con logout funcionando

--- frontend/integrador_3/src/components/Footer.jsx
+++ frontend/integrador_3/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export const Footer = () => {
   return (
     <footer className="bg-gray-700 h-10 w-screen">
-        <p className="text-center"> © 2025 IPF | Todos los derechos reservados </p>
+        <p className="text-center"> © 2025 IPF - Cabrera Dylan | Todos los derechos reservados </p>
     </footer>
   )
 }
--- frontend/integrador_3/src/components/Navbar.jsx
+++ frontend/integrador_3/src/components/Navbar.jsx
@@ -1,16 +1,69 @@
-import { Link } from "react-router"
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router"
 
 
 export const Navbar = () => {
+  const [navStatus, setNavStatus] = useState("loading");
+  const navigate = useNavigate()
+
+  const getProfile = async () => {
+
+    try {
+      const response = await  fetch("http://localhost:3000/api/profile",
+        {
+          credentials: "include",
+        }
+      )
+      if(response.ok) {
+        setNavStatus("authenticated")
+      } else {
+        setNavStatus("unauthenticated")
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(()=>{
+    getProfile()
+  }, [])
+
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await fetch("http://localhost:3000/api/logout",
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      )
+    if(response.ok) {
+      navigate("/login")
+      alert("Logout exitoso")
+    }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <nav className="flex justify-between h-10 max-h-screen bg-gray-600 items-center">
+      {
+        navStatus == "authenticated" ? (
+          <>
             <button> <Link to="/home"> Home </Link> </button>            
             <button> <Link to="/profile"> Profile </Link> </button>            
             <button> <Link to="/tasks"> Tasks </Link> </button>            
+            <button onClick={handleLogout} > Logout </button>
+          </>) : (<>
             <button> <Link to="/login"> Login </Link> </button>            
             <button> <Link to="/register"> Register </Link> </button>            
-            <button > Logout </button>
+          </>)
+      }
             
     </nav>
   )
